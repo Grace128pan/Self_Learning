ea8e36543bc5cb5697f56d90fc5f134a40f85879
Zhen Pan <gracepan922@gmail.com>
2024-07-14 12:06:52 +0300

    java

---
+++ JPrograms/Main.java
@@ -0,0 +1,13 @@
+/*
+1. This is the first test java program. Great job. Just install AdoptOpenJDK and add environemntal path via downloading, then 
+run this program, delcaring the package name different from java. Then it is done
+ */
+
+package JPrograms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
