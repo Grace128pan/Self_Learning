7a1114fdfc9f4c52da9a68cafd390009c7c91660
Zhen Pan <gracepan922@gmail.com>
2024-07-15 17:38:37 +0300

    shortCircuit

---
+++ JPrograms/LogicalOper.java
@@ -0,0 +1,41 @@
+/*
+1. relational or logical operators are shown below
+operator      meaning
+==            equal to
+!=            not equal to
+>             greater than
+<             less than
+>=            greater than or equal to
+<=            less than or equal to
+&             logical and
+|             logical or
+!             logical not
+^             logical xor(exclusive or)
+||            short-circuit or    // short-circuit means if first condition is true then it will not check the second condition
+&&            short-circuit and   // short-circuit means if first condition is true then it will not check the second condition
+In logical operations, the XOR (exclusive or) operator (^) returns true if and only if exactly one of the operands is true. If both operands are true or both are false, the result is false.
+ */
+package JPrograms;
+
+public class LogicalOper {
+    public static void main(String[] args) {
+        int i, j;
+        boolean b1, b2;
+
+        i = 10;
+        j = 11;
+        if(i < j) System.out.println("i < j");
+        if(i <= j) System.out.println("i <= j");
+        if(i !=j) System.out.println("i != j");
+        if(i ==j) System.out.println("this won't execute");
+        if(i >=j) System.out.println("this won't execute");
+        if (i > j) System.out.println("This won't execute");
+
+        b1 = true;
+        b2 = false;
+        if(b1 & b2) System.out.println("This won't execute");
+        if(!(b1 & b2)) System.out.println("!(b1 & b2) is true");
+        if(b1 | b2) System.out.println("b1 | b2 is true");
+        if(b1 ^ b2) System.out.println("b1 ^ b2 is true");
+    }
+}
--- JPrograms/VarLife.java
+++ JPrograms/VarLife.java
@@ -2,6 +2,11 @@
 1. Java has 4 general classes of operators: arithmetic, bitwise, relational, and logical
 2. arithmetic operators: +, -, *, /, %, ++, --
 3. % means we get the remainder of the division; if interger / integer, then result is integer, and the remainder is lost
+4. ++ and -- are unary operators; they can be prefix or postfix
+5. prefix: ++x, --x; postfix: x++, x--
+6. prefix: increment/decrement the variable before using it; postfix: use the variable and then increment/decrement it
+for example, x=10; y= ++x; in this case, y will be set to 11, and x will be set to 11
+x = 10; y=x++; in this case, y will be set to 10, and x will still be set to 11
  */
 
 package JPrograms;
