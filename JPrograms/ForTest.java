5bb39aa638ae1359fe829d0b8146ee2597059d67
Zhen Pan <gracepan922@gmail.com>
2024-07-17 16:51:05 +0300

    scope

---
+++ JPrograms/ForTest.java
@@ -0,0 +1,17 @@
+/*
+1. the condition controlling the loop can be any valid boolean expression. It does not need to involve the loop control variable.
+2. This program will continue to execute until the user types the letter S at the keyboard.
+ */
+
+ package JPrograms;
+
+ public class ForTest {
+        public static void main(String[] args) 
+            throws java.io.IOException {
+                System.out.println("Press S to stop.");
+
+                for (int i=0; (char)System.in.read()!= 'S'; i++) {
+                    System.out.println("Pass #" + i);
+                }
+            }
+ }
---
+++ JPrograms/MissingIter.java
@@ -0,0 +1,30 @@
+/*
+ 1. when the iteration condition in for loop is missing, the loop can still run with fixes inside the loop
+ 2. This program will demonstate part of the for loop is missing
+ */
+
+ package JPrograms;
+
+ public class MissingIter {
+    public static void main(String[] args) {
+        for (int i=0; i<10;) {
+            System.out.println("Pass #" + i);
+            i++; //increment loop control over var
+        }
+
+        //loops with no body
+        int sum = 0;
+        for (int m=0; m<=5; sum+=m++);
+        System.out.println("Sum is: " + sum);
+
+        //when the variable is declared inside the for statement, it can not be used outside the loop
+        int summation = 0;
+        int fact = 1;
+        for(int j = 1; j<=5; j++) {
+            summation += j;
+            fact *= j;
+        }
+        System.out.println("Summation is: " + summation);
+        System.out.println("Factorial is: " + fact);
+    }
+ }
