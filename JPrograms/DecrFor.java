56ef765f62adac9b1e811da8243f1061b7fce929
Zhen Pan <gracepan922@gmail.com>
2024-07-17 15:49:01 +0300

    decrementFor

---
+++ JPrograms/DecrFor.java
@@ -0,0 +1,16 @@
+/*
+ For loop can work in a positive or negative fashion
+ */
+package JPrograms;
+
+public class DecrFor {
+    public static void main(String[] args) {
+        for(int i=100; i> -100; i-= 5) {
+            System.out.println(i);
+        }
+
+        for(int m=0, n=10; m < n; m++, n--) {
+            System.out.println("m and n: " +m + " " + n);
+        }
+    }
+}
