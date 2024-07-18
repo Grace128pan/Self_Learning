24245c308371431103b0da18fbf5163b085a89cc
Zhen Pan <gracepan922@gmail.com>
2024-07-18 18:18:52 +0300

    breakwithlabel

---
+++ JPrograms/BreakWithlabel2.java
@@ -0,0 +1,32 @@
+/*
+    * This program demonstrates the use of break statement with label.
+ */
+
+ package JPrograms;
+
+ public class BreakWithlabel2 {
+    public static void main(String[] args) {
+        for(int i=1; i<4; i++) {
+            one:
+            {
+                two:
+                {
+                    three:
+                    {
+                        System.out.println("\ni is " + i);
+                        if(i ==1) break one;
+                        if(i ==2) break two;
+                        if(i ==3) break three;
+
+                        //this is never reached
+                        System.out.println("This won't print");
+                    }
+                    System.out.println("After block three.");
+                }
+                System.out.println("After block two.");
+            }
+            System.out.println("After block one.");
+        }
+        System.out.println("After for loop.");
+    }
+ }
---
+++ JPrograms/BreakWithlabel3.java
@@ -0,0 +1,21 @@
+/*
+1. This is another example of using break with label.
+ */
+package JPrograms;
+
+public class BreakWithlabel3 {
+    public static void main(String[] args) {
+        done:
+            for(int i=0; i<10; i++) {
+                for(int j=0; j<10; j++) {
+                    for(int k=0; k<10; k++) {
+                        System.out.println(k + " ");
+                        if(k==5) break done;
+                        }
+                        System.out.println("after k loop."); // This line will not be printed.
+                    }
+                    System.out.println("after j loop."); // This line will not be printed.
+                }
+                System.out.println("after i loop.");
+            }
+    }
