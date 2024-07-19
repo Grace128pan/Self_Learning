1982a8ef8985a834bb8ae9c15b13eb1e2f2a1011
Zhen Pan <gracepan922@gmail.com>
2024-07-19 13:20:49 +0300

    factor

--- JPrograms/NestedLoop.java
+++ JPrograms/NestedLoop.java
@@ -1,5 +1,17 @@
-package JPrograms;
+/*
+ use nested loop to find factors of numbers between 2 and 100
+ */
 
-public class NestedLoop {
-    
-}
+ package JPrograms;
+ 
+ public class NestedLoop {
+    public static void main(String[] args) {
+        for(int i=2; i<=100; i++) {
+            System.out.print("Factor of " + i + ": ");
+            for(int j=2; j<i; j++) 
+                if(i%j == 0) System.out.print(j + " ");
+            System.out.println();
+            
+        }
+    }
+ }
---
+++ JPrograms/NestedLoop2.java
@@ -0,0 +1,23 @@
+/*
+1. this program will give a more efficent way of finding factors using square root way
+ */
+
+ package JPrograms;
+
+ public class NestedLoop2 {
+    public static void main(String[] args) {
+        for(int i=2; i<=100; i++) {
+            System.out.print("Factors of " + i + " : ");
+            for(int j=2; j<=Math.sqrt(i); j++) {
+                if(i%j == 0) {
+                    System.out.print(j + " ");
+                    if ( j != i/j) {
+                        System.out.print((i/j) + " "); // to avoid printing same factor twice
+                    }
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+ }
