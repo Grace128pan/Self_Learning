3043e689531d227ff0bf3a4c708416287c883a2a
Zhen Pan <gracepan922@gmail.com>
2024-07-19 14:41:16 +0300

    spacecount

--- JPrograms/SpaceCount.java
+++ JPrograms/SpaceCount.java
@@ -1,5 +1,24 @@
-package JPrograms;
+/*
+ 1.Write a program that reads characters from the keyboard until a period is received. have the program
+ count the number of spaces. Report the total at the end of the program.
+ */
 
-public class SpaceCount {
-    
-}
+ package JPrograms;
+
+ public class SpaceCount {
+        public static void main(String[] args)
+           throws java.io.IOException {
+            char ch;
+            int count = 0;
+
+            System.out.println("Enter characters. To stop, please enter a period('.').");
+
+            do {
+                ch = (char) System.in.read();
+                if (ch == ' ')
+                    count ++;
+            } while(ch != '.');
+
+            System.out.println("Total number of spaces: " + count);
+           }
+ }
