e3ec9e15a207799ef6c0ae29881f978f63c5ff19
Zhen Pan <gracepan922@gmail.com>
2024-07-20 09:31:09 +0300

    upplower

--- JPrograms/UpperLower.java
+++ JPrograms/UpperLower.java
@@ -1,5 +1,9 @@
-package JPrograms;
-
-public class UpperLower {
-    
-}
+/*
+This program should convert lowercase letters to uppercase and uppercase letters to lowercase.
+The ASCII lowercaseletters are in the range 97 to 122, and the ASCII uppercase letters are in the range 65 to 90.
+So, to convert a lowercase letter to uppercase, you subtract 32 from its ASCII value.
+1. Use the logic to convert lowercase to uppcase or uppercase to lowercase, making no changes to any other character
+2. Have the program stop when a period is entered
+3. At the end the program will display the number case changes that have taken place
+4. The program should read characters from the keyboard
+ */
