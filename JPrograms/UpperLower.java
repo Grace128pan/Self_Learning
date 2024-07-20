3ea600a568562899e54f836418a29d25a07985a0
Zhen Pan <gracepan922@gmail.com>
2024-07-20 09:31:19 +0300

    1

--- JPrograms/UpperLower.java
+++ JPrograms/UpperLower.java
@@ -7,3 +7,36 @@ So, to convert a lowercase letter to uppercase, you subtract 32 from its ASCII v
 3. At the end the program will display the number case changes that have taken place
 4. The program should read characters from the keyboard
  */
+
+ package JPrograms;
+
+ public class UpperLower {
+     public static void main(String[] args)
+         throws java.io.IOException {
+             char ch;
+             int count = 0;
+ 
+             System.out.println("Enter characters. Enter a period '.' to stop.");
+ 
+             do {
+                 ch = (char) System.in.read();
+ 
+                 if(ch >= 'a' && ch <= 'z') {
+                     ch = (char) (ch - 32);
+                     count++;
+                 } else if(ch >= 'A' && ch <= 'Z') {
+                     ch = (char) (ch + 32);
+                     count++;
+                 }
+ 
+                 // Skip newline and carriage return characters and the period
+                 if(ch != '\n' && ch != '\r' && ch != '.') {
+                     System.out.print(ch);
+                 }
+ 
+             } while(ch != '.');
+ 
+             System.out.println("\nNumber of case changes: " + count);
+         }
+ }
+ 
