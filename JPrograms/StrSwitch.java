f0e725666664fb4f985f21b2a4eeb265ac029296
Zhen Pan <gracepan922@gmail.com>
2024-07-23 21:59:12 +0300

    StringCommand

---
+++ JPrograms/StrSwitch.java
@@ -0,0 +1,26 @@
+/*
+Using a stirng to control a switch statement
+
+The following program demonstrates how to use a string to control a switch
+ */
+
+public class StrSwitch {
+    public static void main(String[] args) {
+        String command = "cancel";
+
+        switch(command) {
+            case "connect":
+                System.out.println("Connecting...");
+                break;
+            case "cancel":
+                System.out.println("Canceling...");
+                break;
+            case "disconnect":
+                System.out.println("Disconnecting...");
+                break;
+            default:
+                System.out.println("Command Error!");
+                break;
+        }
+    }
+}
