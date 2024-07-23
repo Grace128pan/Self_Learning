4c70a229b37d73385ea0eba7167b7a05343563a8
Zhen Pan <gracepan922@gmail.com>
2024-07-23 14:14:30 +0300

    search

--- JPrograms/search.java
+++ JPrograms/search.java
@@ -1,3 +1,22 @@
+/*
+1. the following program uses a for loop to search an unsorted array for a value. It stops if the value is found.
+ */
+
 public class search {
-    
+    public static void main(String[] args) {
+        int[] nums = { 6, 8, 3, 7, 5, 6, 1, 4 };
+        int value = 5;
+        boolean found = false;
+
+        //use for-each loop style to search nums for val
+        for(int x: nums) {
+            if(x==value) {
+                found = true;
+                break;
+            }
+        }
+        if(found) {
+            System.out.println("Value has been found! Congraduations!");
+        }
+    }
 }
