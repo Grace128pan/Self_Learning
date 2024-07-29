b8238abbbb18af958dc33e2973dc8c1b623ea8c6
Zhen Pan <gracepan922@gmail.com>
2024-07-29 19:27:00 +0300

    showbits

---
+++ JPrograms/Chapter6.java
@@ -0,0 +1,19 @@
+/*
+chapter 6
+a closer look at methods and classes
+
+key skills and concepts
+1)control access to methods
+2)pass objects to a method
+3)overload methods
+4)overload constructors
+5)use recursion
+6)apply static 
+7)use inner classes
+8)use varargs
+ */
+
+
+public class Chapter6 {
+    
+}
