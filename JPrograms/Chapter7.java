4eb225815b0474f7cadb9c8abb3661bbc7535ec5
Zhen Pan <gracepan922@gmail.com>
2024-08-13 22:31:40 +0300

    ambiguity

---
+++ JPrograms/Chapter7.java
@@ -0,0 +1,19 @@
+/*
+ chapter 7: inheritance
+ 1.understand inheritance basics
+ 2.call superclass constructors
+ 3.use superclass constructors
+ 4.create a multilevel class hierarchy
+ 5.know when constructors are called
+ 6.understand superclass references to subclass objects
+ 7.overide methods
+ 8.use overridden methods to achieve dynamic method dispatch
+ 9.use abstract classed
+ 10.use final
+ 11.know the object class
+ */
+
+
+public class Chapter7 {
+    
+}
