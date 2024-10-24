39ae87fdc1a0f9a32a89e12eacafaf844b024b92
Zhen Pan <gracepan922@gmail.com>
2024-10-24 16:40:07 +0300

    dictionary

---
+++ VPythonCourse/10dictionary.py
@@ -0,0 +1,15 @@
+#dictionary 
+'''
+Collection of Key - Value pairs
+also known as associative array
+also known as map, hashmap
+unordered
+keys unique in one dictionary
+storing, extracting
+'''
+student = {("name", "alice"), ("age", 20),("grade", "A")}
+
+#dictionaries are mutable
+student1 = {"name": "alice", "age": 20, "grade": "A"}
+print(student)
+print(student1)
